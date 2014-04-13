8a747278c7807d2be9922d35873afc9566d7948d
negipo <negipo@gmail.com>
2014-04-13 09:29:50 +0900

    Add markdown contextmenu

--- extension/lgtmify.js
+++ extension/lgtmify.js
@@ -9,9 +9,20 @@ function imageHandler(info, tab) {
   copy('http://lgtm.herokuapp.com/' + info.srcUrl);
 }
 
+function imageWithMarkdownHandler(info, tab) {
+  copy('[LGTM](http://lgtm.herokuapp.com/' + info.srcUrl + ')');
+}
+
 chrome.contextMenus.create({
   'title' : 'Copy image link with LGTM',
   'type' : 'normal',
   'contexts' : ['image'],
   'onclick' : imageHandler
+});
+
+chrome.contextMenus.create({
+  'title' : 'Copy image link with LGTM as markdown',
+  'type' : 'normal',
+  'contexts' : ['image'],
+  'onclick' : imageWithMarkdownHandler
 });
