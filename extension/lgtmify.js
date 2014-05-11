0e596abe3617bb35891fefbdebb3ed818f5c17ed
negipo <negipo@gmail.com>
2014-05-11 14:20:29 +0900

    Add LGTM with comments

--- extension/lgtmify.js
+++ extension/lgtmify.js
@@ -13,16 +13,38 @@ function imageWithMarkdownHandler(info, tab) {
   copy('[![LGTM](http://lgtm.herokuapp.com/' + info.srcUrl + ')](http://lgtm.herokuapp.com/)');
 }
 
+function imageWithCommentsHandler(info, tab) {
+  copy('http://lgtm.herokuapp.com/with_comments/' + info.srcUrl);
+}
+
+function imageWithCommentsAndMarkdownHandler(info, tab) {
+  copy('[![LGTM](http://lgtm.herokuapp.com/with_comments/' + info.srcUrl + ')](http://lgtm.herokuapp.com/)');
+}
+
 chrome.contextMenus.create({
-  'title' : 'Copy image link with LGTM',
+  'title' : 'Copy image link with "LGTM"',
   'type' : 'normal',
   'contexts' : ['image'],
   'onclick' : imageHandler
 });
 
 chrome.contextMenus.create({
-  'title' : 'Copy image link with LGTM as markdown',
+  'title' : 'Copy image link with "LGTM" as markdown',
   'type' : 'normal',
   'contexts' : ['image'],
   'onclick' : imageWithMarkdownHandler
+});
+
+chrome.contextMenus.create({
+  'title' : 'Copy image link with "LGTM with comments"',
+  'type' : 'normal',
+  'contexts' : ['image'],
+  'onclick' : imageWithCommentsHandler
+});
+
+chrome.contextMenus.create({
+  'title' : 'Copy image link with "LGTM with comments" as markdown',
+  'type' : 'normal',
+  'contexts' : ['image'],
+  'onclick' : imageWithCommentsAndMarkdownHandler
 });
